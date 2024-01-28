96826c69815c3e6f8812be4865fdc98e0d4c5d7a
Nuttachai Puksasuk <Nuttachai.work@gmail.com>
2024-01-28 23:44:19 +0700

    Revert "code init"
    This reverts commit 9fd7d4c83515d6f0f9b31eced07544e9e10b9be5.

--- app/models/notification.server.ts
+++
@@ -1,9 +0,0 @@
-export interface NotificationDetail {
-  itemNo: number
-  notificationId: string
-  notificationAt: string
-  imgUrl: string
-  name: string
-  totalPoint: number
-  status: string //pening , success 
-}
--- app/routes/_index.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderFunctionArgs, redirect } from "@remix-run/node";
-
-import { getUserId } from "~/services/session.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const userId = await getUserId(request);
-  if (userId) return redirect("/customers");
-  return redirect("/login");
-};
