202dc54809641845325001ea9627c1245b9adc40
porperza <pikza_classic@hotmail.com>
2025-03-04 01:10:46 +0700

    UserCheckin

---
+++ app/components/CustomerCheckinTable.tsx
@@ -0,0 +1,258 @@
+import { useReactTable, getCoreRowModel, getPaginationRowModel, flexRender, createColumnHelper } from "@tanstack/react-table";
+import { FC, useState, useRef, useEffect, useMemo } from "react";
+import {
+    Form,
+    useActionData,
+    useNavigate,
+    useRevalidator,
+    useSearchParams,
+    useSubmit,
+} from "@remix-run/react";
+import EmptyState from "./EmptyState";
+import { convertUTC } from "~/utils";
+import PaginationNavigator from "./PaginationNavigator";
+import { useLoaderData, useLocation } from "@remix-run/react";
+import Datepicker from "react-tailwindcss-datepicker";
+import { Prettify } from "~/utils.type";
+import { CustomersDataWithItemNo, CustomersResponse } from "~/models/customerEvent.server";
+import DropdownSelect from "./DropdownSelect";
+import Search from "./Search";
+import { classNames } from "~/tailwind";
+import UsernameSection from "./UserNameSection";
+import { parseISO, format, subHours } from "date-fns";
+
+import axios from "axios";
+import { getUserData, requireUserId } from "~/services/session.server";
+import { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
+import CheckinUpdateStatus from './CheckinUpdateStatus.jsx';
+
+
+const timeZoneOffset = 7;
+
+interface CustomerCheckinTableProps {
+    data: CustomersResponse;
+    filter: string;
+    setFilter: React.Dispatch<React.SetStateAction<string>>;
+    accessToken: string;
+}
+
+const columnHelper = createColumnHelper<CustomersDataWithItemNo>(); // Use correct type
+
+export type CustomersProps = Prettify<
+    Omit<CustomersResponse, "data"> & { data: CustomersDataWithItemNo[] }
+>;
+
+interface DateType {
+    startDate: Date | string;
+    endDate: Date | string;
+}
+
+function CustomerCheckinTable({
+    data,
+    filter,
+    setFilter,
+    accessToken
+}: CustomerCheckinTableProps): JSX.Element {
+
+    const [searchParams] = useSearchParams();
+    const today = new Date();
+    const sevenDaysAgo = new Date(today);
+    sevenDaysAgo.setDate(today.getDate() - 7);
+    const defaultDate = {
+        startDate: format(sevenDaysAgo, "yyyy-MM-dd"),
+        endDate: format(today, "yyyy-MM-dd"),
+    };
+
+    const [dateValue, setValue] = useState<DateType>(defaultDate);
+
+    function handleValueChange(newValue: DateType) {
+        console.log("handleValueChange", newValue);
+        setValue(newValue);
+    }
+
+
+    const [, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        setSearchParams(
+            (prev) => {
+                const updatedSearchParams = new URLSearchParams(prev);
+                if (dateValue?.startDate) {
+                    // start date
+                    updatedSearchParams.set(
+                        "startAt",
+                        convertUTC({ dateValue: dateValue.startDate, isStart: true }),
+                    );
+                } else {
+                    updatedSearchParams.delete("startAt");
+                }
+                if (dateValue?.endDate) {
+                    // end date
+                    updatedSearchParams.set(
+                        "endAt",
+                        convertUTC({ dateValue: dateValue.endDate }),
+                    );
+                } else {
+                    updatedSearchParams.delete("endAt");
+                }
+
+                return updatedSearchParams;
+            },
+            { preventScrollReset: true },
+        );
+    }, [dateValue.endDate, dateValue.startDate, setSearchParams]);
+
+    const columns = useMemo(
+        () => [
+            columnHelper.accessor("itemNo", {
+                header: () => "No.",
+                cell: (info) => info.getValue(),
+            }),
+            columnHelper.accessor("restaurant_name", {
+                header: () => "ชื่อร้าน",
+                cell: (info) => info.getValue() ?? "-",
+            }),
+            columnHelper.accessor("branch_name", {
+                header: () => "พิกัส / โซน",
+                cell: (info) => info.getValue() ?? "-",
+            }),
+            columnHelper.accessor("created_at", {
+                header: () => "วัน-เวลา Check-in",
+                cell: (info) => {
+                    const date = parseISO(info.getValue());
+                    const zonedDate = subHours(date, timeZoneOffset);
+                    return format(zonedDate, 'dd MMMM yyyy เวลา HH:mm');
+                },
+            }),
+            columnHelper.accessor((row) => ({ name: row.customer_name, image: row.customer_picture }), {
+                header: "ชื่อผู้ใช้",
+                cell: (info) => (
+                    <UsernameSection
+                        name={info.getValue().name}
+                        imageUrl={info.getValue().image}
+                    />
+                ),
+                size: 44,
+            }),
+            columnHelper.accessor("image_url", {
+                header: () => "รายละเอียด",
+                cell: (info) => {
+                    const imgSrc = info.getValue() ?? "-"; //info.getValue() ??
+                    return imgSrc !== "-" ? (
+                        <img src={imgSrc} alt="Customer" width={50} height={50} />
+                    ) : (
+                        <span>-</span>
+                    );
+
+                },
+            }),
+            columnHelper.accessor("status", {
+                header: () => "ตรวจสอบ",
+                cell: (info) => {
+                    return (
+                        <CheckinUpdateStatus
+                            id={info.row.original.id}
+                            approve_status={info.getValue()}
+                            accessToken={accessToken}
+                        />
+                    )
+                },
+            }),
+        ],
+        [data],
+    )
+    // Map the `data.data` to include `itemNo`
+    const mappedData: CustomersDataWithItemNo[] = data.data.map((customer, index) => ({
+        ...customer,
+        itemNo: (data.currentPage - 1) * data.perPage + index + 1, // Calculate `itemNo`
+    }));
+
+    const table = useReactTable({
+        data: data.data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+    });
+
+
+
+    return (
+        <div className="w-full h-full flex flex-col ">
+            <div className="flex justify-between items-end mb-4">
+                <p>ผู้เล่นเกมส์ทั้งหมด ({data.totalRow})</p>
+                <div className="flex flex-row gap-x-3">
+                    <Datepicker
+                        primaryColor={"blue"}
+                        value={dateValue!}
+                        onChange={(value) => handleValueChange(value as DateType)}
+                    />
+                    <Search filter={filter} setFilter={setFilter} />
+                </div>
+            </div>
+            {/* Your table rendering */}
+            <div
+                className={classNames(
+                data.totalRow > 0 ? "overflow-y-auto" : "",
+                "md:h-[12.25rem] lg:max-h-[40rem] flex-grow border-gray-400 bg-white",
+                )}
+            >
+                <table className="w-full">
+                    <thead>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <th
+                                        key={header.id}
+                                        className={classNames(
+                                            `md:max-w-[${header.getSize()}px]`,
+                                            "flex-grow flex-shrink-0 h-[2.75rem] px-2.5 py-1 bg-gray-200 border-t border-b border-gray-400 justify-start items-center gap-2.5 text-slate-500 text-base text-left font-light font-roboto",
+                                        )}
+                                    >
+                                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody>
+                        {table.getRowModel().rows.map((row) => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <td
+                                        key={cell.id}
+                                        className={classNames(
+                                            "flex-grow flex-shrink-0 h-[2.8rem] px-2 py-1 bg-white border-b border-gray-400 justify-start gap-2.5 text-stone-800 text-sm font-normal font-roboto",
+                                        )}
+                                    >
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                {data.totalRow === 0 ? (
+                <div className="flex h-full justify-center items-center ">
+                    <EmptyState />
+                </div>
+                ) : null}
+            </div>
+
+            {/* Pagination */}
+            <div className="flex justify-between items-center gap-2 mt-2 mb-16">
+                <span className="flex items-center gap-1">
+                    <div>Page</div>
+                    <strong>{table.getState().pagination.pageIndex + 1} of {table.getPageCount()}</strong>
+                </span>
+
+                <PaginationNavigator
+                    currentPage={table.getState().pagination.pageIndex + 1}
+                    totalPage={table.getPageCount()}
+                    setPageIndex={table.setPageIndex}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default CustomerCheckinTable;
--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -47,6 +47,27 @@ const generalNavigation: Navigation[] = [
   },
 ];
 
+const eventNavigation: Navigation[] = [
+  {
+    name: "Checkinuser",
+    text: "ข้อมูลการ Check-in",
+    icon: "/images/map-pin.svg",
+    href: "/checkinusers",
+  },
+  {
+    name: "Set Box",
+    text: "การแลก Set Box",
+    icon: "/images/ph_storefront-light.svg",
+    href: "/SetBoxsettings",
+  },
+  {
+    name: "Top 100",
+    text: "Top 100",
+    icon: "/images/users.svg",
+    href: "/Top-100",
+  },
+];
+
 const gameNavigation: Navigation[] = [
   {
     name: "Player",
@@ -79,7 +100,7 @@ export default function Sidebar({
   openModal,
 }: SidebarProps): JSX.Element {
   return (
-    <div className="flex flex-col w-64 bg-white text-white shadow-md z-10 h-[110vh]">
+    <div className="flex flex-col w-64 bg-white text-white shadow-md z-10">
       <div className="flex flex-col items-center justify-center h-32 mt-8 mb-8">
         <img
           src="/images/brand-logo.svg"
@@ -98,8 +119,8 @@ export default function Sidebar({
         </div>
       </div>
 
-      <div className="flex-1">
-        <ul>
+      <div className="flex-1 overflow-auto h-[100vh]">
+        <ul >
           <li className="mx-5 my-2.5 p-2">
             <p className="text-blackrose font-semibold">General Setting</p>
           </li>
@@ -129,6 +150,44 @@ export default function Sidebar({
                   className={classNames(
                     isCurrentTab ? "hover:text-white" : "",
                     "font-roboto font-normal text-blackrose",
+                  )}
+                >
+                  {navigation.text}
+                </Link>
+              </li>
+            );
+          })}
+
+          <hr className="border-t border-philippine-silver mx-5 my-2.5 h-2.5 mt-4" />
+
+          <li className="mx-5 my-2.5 p-2">
+            <p className="text-blackrose font-semibold">Event Setting</p>
+          </li>
+
+          {eventNavigation.map((navigation) => {
+            const isCurrentTab = pathname.startsWith(
+              "/" + navigation.name.toLowerCase(),
+            );
+            return (
+              <li
+                key={navigation.name}
+                className={classNames(
+                  isCurrentTab ? "bg-charged-blue" : "bg-white",
+                  "flex mx-5 my-[5px] rounded-md p-2 leading-6",
+                )}
+              >
+                <img
+                  src={navigation.icon}
+                  alt={navigation.name}
+                  className="w-6 h-6 self-center ml-2.5 mr-4"
+                  draggable="false"
+                />
+
+                <Link
+                  to={navigation.href}
+                  className={classNames(
+                    isCurrentTab ? "hover:text-white" : "",
+                    "font-normal font-roboto text-blackrose",
                   )}
                 >
                   {navigation.text}
@@ -176,24 +235,24 @@ export default function Sidebar({
             );
           })}
 
-          <hr className="border-t border-philippine-silver mx-5 my-2.5 h-2.5 mt-4" />
+          
         </ul>
-      <div className="bg-white flex mx-5 mb-6 rounded-md p-2 leading-6 items-center">
-        <img
-          src="/images/logout.svg"
-          alt="Logout"
-          className="w-6 h-6 self-center ml-2.5 mr-4"
-          draggable="false"
-        />
+      </div>
+        <div className="bg-white flex mx-5 mb-6 rounded-md p-2 leading-6 items-center">
+          <img
+            src="/images/logout.svg"
+            alt="Logout"
+            className="w-6 h-6 self-center ml-2.5 mr-4"
+            draggable="false"
+          />
 
-        <button
-          onClick={openModal}
-          className="text-blackrose text-sm font-normal font-kanit"
-        >
-          Logout
-        </button>
-      </div>
-      </div>
+          <button
+            onClick={openModal}
+            className="text-blackrose text-sm font-normal font-kanit"
+          >
+            Logout
+          </button>
+        </div>
 
     </div>
   );
